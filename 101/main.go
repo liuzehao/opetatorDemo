f98dbc119fb72bde470a1bde953fda24faf2ad02
Zehao Liu <zehao.liu@shopee.com>
2024-01-18 00:48:56 +0800

    add list&watch

--- 101/main.go
+++ 101/main.go
@@ -27,14 +27,14 @@ func main() {
 
 	podLW := cache.NewListWatchFromClient(clientset.CoreV1().RESTClient(), "pods", "default", fields.Everything())
 	//list function
-	//list, err := podLW.List(metav1.ListOptions{})
-	//if err != nil {
-	//	log.Fatalln(err)
-	//}
-	//podList := list.(*v1.PodList)
-	//for _, pod := range podList.Items {
-	//	fmt.Printf(pod.Name)
-	//}
+	list, err := podLW.List(metav1.ListOptions{})
+	if err != nil {
+		log.Fatalln(err)
+	}
+	podList := list.(*v1.PodList)
+	for _, pod := range podList.Items {
+		fmt.Printf(pod.Name)
+	}
 
 	//	watch function
 	watcher, err := podLW.Watch(metav1.ListOptions{})
